8a8b52a2bbff9b8c429207209cdfc0b7946771c0
AbnerMartinez01 <122928340+AbnerMartinez01@users.noreply.github.com>
2023-03-29 23:59:49 -0600

    Added Sidebar

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material"
 import Head from "next/head"
 import { FC, ReactNode } from "react"
 import { NavBar } from "../ui";
+import SideBar from "../ui/SideBar";
 
 // sx tiene acceso al tema, por lo tanto se puede alterar desde el layout
 
@@ -18,7 +19,7 @@ export const Layout: FC<Props> = ({ title = 'OpenJira - App', children }) => {
         </Head>
 
         <NavBar />
-        {/*Side bar*/ }
+        <SideBar/>
 
         <Box sx={{padding: '10px 20px'}}>
             {children}
---
+++ components/ui/SideBar.tsx
@@ -0,0 +1,47 @@
+import { Box, Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material"
+import InboxOutlinedIcon from '@mui/icons-material/InboxOutlined';
+import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
+const menuItems: string[] = ['Inbox', 'Starred', 'Send Email', 'Draft']
+
+const SideBar = () => {
+  return (
+    <Drawer
+        anchor="left"
+        open={true}
+        onClose={()=> console.log("cerrando sidebar")}
+    >
+        <Box sx={{ width: 250}}>
+            <Box sx={{padding: '5px 10px'}}>
+                <Typography variant="h4" >Menu</Typography>
+            </Box>
+            <List>
+                {
+                    menuItems.map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>
+                                {index % 2 ? <InboxOutlinedIcon/> :<MailOutlineOutlinedIcon/> } 
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    ))
+                }
+            </List>
+            <Divider/>
+            <List>
+                {
+                    menuItems.map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>
+                                {index % 2 ? <InboxOutlinedIcon/> :<MailOutlineOutlinedIcon/> } 
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    ))
+                }
+            </List>            
+        </Box>
+    </Drawer>
+  )
+}
+
+export default SideBar
